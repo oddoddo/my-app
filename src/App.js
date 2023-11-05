03fb61807d71267370a1818b07400eb9ca5ba012
oddoddo <eehd80yul@gmail.com>
2023-11-06 07:38:54 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,38 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 function App() {
     const navArr = [{ title: 'html' }, { title: 'css' }, { title: 'js' }];
+
+    const [currentPage, setCurrentPage] = useState('home'); // 초기 페이지는 'home'
+
+    const handleNavClick = (title) => {
+        setCurrentPage(title);
+    };
+
+    const getPageContent = () => {
+        if (currentPage === 'home') {
+            return <div>Hello</div>;
+        } else {
+            const selectedArticle = articleArr.find((item) => item.title === currentPage);
+            return <Article title={selectedArticle.title} desc={selectedArticle.desc} />;
+        }
+    };
+
     return (
         <div className="root">
-            <HeaderStyled title="리액트(React)" nav={navArr} />
-            <Article title="리액트란?" desc="desc1" />
-            <Article title="컴포넌트란?" desc="desc2" />
-            <Article title="props란?" desc="desc3" />
+            <HeaderStyled title="리액트(React)" nav={navArr} onNavClick={handleNavClick} />
+            {getPageContent()}
         </div>
     );
 }
 
-function Header({ title, nav }) {
-    // props.nav = [{title: 'html'}, {title: 'css'}, {title: 'js'}]
-    // const { title, nav } = props;
+function Header({ title, nav, onNavClick }) {
     return (
         <header>
             <h1>{title}</h1>
-            <Nav nav={nav} />
+            <Nav nav={nav} onNavClick={onNavClick} />
         </header>
     );
 }
 
-function Nav({ nav }) {
-    // props.nav = [{title: 'html'}, {title: 'css'}, {title: 'js'}]
-    // const { nav } = props;
+function Nav({ nav, onNavClick }) {
     return (
         <nav>
             <ul>
                 {nav.map((item, index) => (
                     <li key={index}>
-                        <a href={'/sub/' + item.title}>{item.title}</a>
+                        <a href="#" onClick={() => onNavClick(item.title)}>
+                            {item.title}
+                        </a>
                     </li>
                 ))}
             </ul>
@@ -54,5 +66,10 @@ const HeaderStyled = styled(Header)`
     border-bottom: 1px solid #eee;
 `;
 
-// App을 기본으로 내보내기
+const articleArr = [
+    { title: 'html', desc: 'html은 모다모다...' },
+    { title: 'css', desc: 'css는 모다모다...' },
+    { title: 'js', desc: 'js는 모다모다...' },
+];
+
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React, { useState } from 'react';
+import './App.css';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+    const [content, setContent] = useState('Hello');
+
+    const handleMenuItemClick = (menuItem) => {
+        // 각 메뉴 항목에 따른 내용을 설정
+        if (menuItem === 'home') {
+            setContent('Hello');
+        } else if (menuItem === 'about') {
+            setContent('About us content goes here');
+        } else if (menuItem === 'contact') {
+            setContent('Contact us content goes here');
+        }
+    };
+
+    return (
+        <div className="App">
+            <h1>{content}</h1>
+            <div>
+                <ul>
+                    <li onClick={() => handleMenuItemClick('home')}>Home</li>
+                    <li onClick={() => handleMenuItemClick('about')}>About</li>
+                    <li onClick={() => handleMenuItemClick('contact')}>Contact</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default App;
