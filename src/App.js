c7c629f7f964eef76e9c28b3271e77cb5cf897a6
odd:odd <eehd80yul@gmail.com>
2024-01-25 10:20:01 +0900

    Create components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+// 함수 선언식
+function Header() {
+    return <Hd>header</Hd>;
+}
+const Hd = styled.div`
+    height: 100px;
+    background-color: yellowgreen;
+`;
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">html</a>
+                </li>
+                <li>
+                    <a href="/">css</a>
+                </li>
+                <li>
+                    <a href="/">js</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+const Article = () => {
+    return (
+        <article>
+            <h2>제목1</h2>
+            <p>본문1</p>
+        </article>
+    );
+};
+
+// 함수 표현식
+// const Footer = function () {
+//     return <Ft>footer</Ft>;
+// };
+
+// 화살표 함수
+const Footer = () => {
+    return <Ft>footer</Ft>;
+};
+
+const Ft = styled.footer`
+    height: 100px;
+    background-color: #eee;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // return 내부에는 하나의 태그만 존재해야 한다.
+    // 여러개의 태그를 사용하고 싶다면, <></>
+    // 또는 <Fragment></Fragment>를 사용한다.
+    // 한 줄 이상일 경우 ()로 감싸준다.
+    return (
+        <div className="wrap">
+            <Header />
+            <Nav />
+            <Article />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
